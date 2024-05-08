507de8710fc42d1c3dcf9b708c00de008b1bcf61
programmer-kabir <programmerkabirr@gmail.com>
2024-05-08 09:25:08 +0600

    server succesfully data send and get

--- Server/app.js
+++ Server/app.js
@@ -1,11 +1,57 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+require("dotenv").config();
+
+// Middle ware
+app.use(cors());
+app.use(express.json());
+
+// TShirtBussiness
+// WXnaBIXl94LNzLWB
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://TShirtBussiness:WXnaBIXl94LNzLWB@cluster0.0i3pjbq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+
+    const TShirtCollection = client.db("TShirtCollection").collection("AllTShirt");
+
+app.get('/all_t_shirt', async(req , res) =>{
+  const allTShirt = await TShirtCollection.find().toArray()
+  res.send(allTShirt)
+})
+
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("MONGODB Connect successfully😊😊😊");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Sever is running 😊😊😊");
 });
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+app.listen(port);
